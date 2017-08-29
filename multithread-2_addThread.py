ac60d67f3fdfdb69e8252bd918998b3f6b588166
PeterLu <zxgpkm@gmail.com>
2017-08-29 17:05:09 +0800

    Update multithread-2_addThread.py

--- multithread-2_addThread.py
+++ multithread-2_addThread.py
@@ -1,20 +1,16 @@
-#add thread方法
-import multiprocessing as mp
+#add thread
+import threading
 
-def job(q):
-    res = 0
-    for i in range(1000):
-        res += i+i**2+i**3
-    q.put(res)# queue
-    
+def thread_job():
+    print('This is an added Thread,number is %s'%threading.current_thread())
+
+def main():
+    added_thread = threading.Thread(target=thread_job)
+    added_thread.start()
+    added_thread.join()
+    #print(threading.active_count())
+    #print(threading.enumerate())
+    #print(threading.current_thread())
+
 if __name__=='__main__':
-    q = mp.Queue()
-    p1=mp.Process(target=job,args=(q,))
-    p2=mp.Process(target=job,args=(q,))
-    p1.start()
-    p2.start()
-    p1.join()
-    p1.join()
-    res1 = q.get()
-    res2 = q.get()
-    print(res1+res2)
+    main()
